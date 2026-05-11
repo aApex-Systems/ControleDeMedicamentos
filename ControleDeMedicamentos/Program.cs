0da15b11eb92cf3163d85ab9fbad35ae8acfedec
Marcus Copacheski <marcusdanielcopacheski@gmail.com>
2026-05-11 16:08:59 -0300

    program atualizada

--- ControleDeMedicamentos/Program.cs
+++ ControleDeMedicamentos/Program.cs
@@ -1,35 +1,63 @@
-﻿
+﻿using System.Text.Json;
+using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Compartilhado.Arquivos;
+using ControleDeMedicamentos.ConsoleApp.ModuloPacientes;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
+using ControleDeMedicamentos.ConsoleApp.Utilidades;
 
+ContextoJson contexto = new ContextoJson();
 
-internal class Program
+try
 {
-    private static void Main(string[] args)
+    contexto.Carregar();
+}
+catch (JsonException)
+{
+    Notificador.ExibirMensagem("O arquivo de armazenamento está corrompido! Contate o suporte.");
+    return;
+}
+
+IRepositorio<Paciente> repositorioPaciente = new RepositorioPacienteEmArquivo(contexto);
+IRepositorio<Fornecedor> repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
+
+TelaPrincipal telaPrincipal = new TelaPrincipal(
+    repositorioPaciente,
+    repositorioFornecedor    
+);
+
+while (true)
+{
+    ITelaOpcoes? telaSelecionada = telaPrincipal.ApresentarMenuOpcoesPrincipal();
+
+    if (telaSelecionada == null)
     {
-        while (true)
+        Console.Clear();
+        break;
+    }
+
+    while (true)
+    {
+        string? opcaoSubMenu = telaSelecionada.ObterOpcaoMenu();
+
+        if (opcaoSubMenu == "S")
         {
-            string? opcaoSubMenu = telaSelecionada.ObterOpcaoMenu();
+            Console.Clear();
+            break;
+        }
 
-            if (opcaoSubMenu == "S")
-            {
-                Console.Clear();
-                break;
-            }
+        if (telaSelecionada is ITelaCrud telaCrud)
+        {
+            if (opcaoSubMenu == "1")
+                telaCrud.Cadastrar();
 
-            if (telaSelecionada is ITelaCrud telaCrud)
-            {
-                if (opcaoSubMenu == "1")
-                    telaCrud.Cadastrar();
+            else if (opcaoSubMenu == "2")
+                telaCrud.Editar();
 
-                else if (opcaoSubMenu == "2")
-                    telaCrud.Editar();
+            else if (opcaoSubMenu == "3")
+                telaCrud.Excluir();
 
-                else if (opcaoSubMenu == "3")
-                    telaCrud.Excluir();
-
-                else if (opcaoSubMenu == "4")
-                    telaCrud.VisualizarTodos(deveExibirCabecalho: true);
-            }
+            else if (opcaoSubMenu == "4")
+                telaCrud.VisualizarTodos(deveExibirCabecalho: true);
         }
     }
 }
-// add comit
